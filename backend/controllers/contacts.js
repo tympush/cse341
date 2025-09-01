c8d2b85f75b09dc4bf6ff85ca8c81342cdaf9814
tympush <tymur.pushnoy@gmail.com>
2025-09-01 19:09:40 +0200

    made a reference to the db that is used

--- backend/controllers/contacts.js
+++ backend/controllers/contacts.js
@@ -2,7 +2,7 @@ const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
-    const result = await mongodb.getDb().db().collection('contacts').find();
+    const result = await mongodb.getDb().db('project1').collection('contacts').find();
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts);
@@ -11,7 +11,7 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
     const contactsId = new ObjectId(req.params.id);
-    const result = await mongodb.getDb().db().collection('contacts').find({ _id: contactsId });
+    const result = await mongodb.getDb().db('project1').collection('contacts').find({ _id: contactsId });
     result.toArray().then((contacts) => {
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts[0]);
